dc3014ec52d05748b2e77d23d93cf6d1a60db813
Ran Pol <rnd809@gmail.com>
2022-07-15 00:34:06 -0400

    Generate Random Whole Numbers within a Range

--- app2.js
+++ app2.js
@@ -11,3 +11,17 @@ function randomFraction() {
 }
 
 console.log(randomFraction())
+
+// Generate Random Whole Numbers with JavaScript
+// Use this technique to generate and return a random whole number between 0 and 9.
+
+function randomWholeNum() {
+    return Math.floor(Math.random() * 10);
+}
+
+// Generate Random Whole Numbers within a Range
+function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin
+    // Only change code above this line
+}
