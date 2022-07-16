dc9b3329f3ab4231a7906c29c1d978627b6debd0
Ran Pol <rnd809@gmail.com>
2022-07-16 06:47:35 -0400

    Write Concise Object Literal Declarations Using Object Property Shorthand

--- ES6/app.js
+++ ES6/app.js
@@ -273,4 +273,20 @@ function makeList(arr) {
 }
 
 const failuresList = makeList(result.failure);
-console.log(failuresList)
+
+
+// 
+// Section 16: Write Concise Object Literal Declarations Using Object Property Shorthand
+// 
+// Use object property shorthand with object literals to create and return an object with 
+// name, age and gender properties.
+
+const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // Only change code above this line
+};
