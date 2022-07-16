8771ce78a6fcf81f6e740ebfadc4ad7a705359b3
Ran Pol <rnd809@gmail.com>
2022-07-16 06:42:18 -0400

    Create Strings using Template Literals

--- ES6/app.js
+++ ES6/app.js
@@ -233,4 +233,44 @@ const stats = {
 // Only change code below this line
 const half = ({ min, max }) => (max + min) / 2.0;
 // Only change code above this line
-console.log(half(stats));
+
+
+// 
+// Section 15: Create Strings using Template Literals
+// 
+// Use template literal syntax with backticks to create an 
+// array of list element (li) strings. Each list 
+// element's text should be one of the array elements 
+// from the failure property on the result object and have 
+// a class attribute with the value text-warning. The makeList
+//  function should return the array of list item strings.
+
+// Use an iterator method (any kind of loop) to get the desired output (shown below).
+
+// [
+//   '<li class="text-warning">no-var</li>',
+//   '<li class="text-warning">var-on-top</li>',
+//   '<li class="text-warning">linebreak</li>'
+// ]
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  arr.forEach(a => {
+    failureItems.push(
+      `<li class="text-warning">${a}</li>`)
+  }
+  )
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+console.log(failuresList)
