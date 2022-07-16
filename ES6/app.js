bb65822d2a3fb25689508972a3575468ad994b0d
Ran Pol <rnd809@gmail.com>
2022-07-16 06:31:10 -0400

    Use Destructuring Assignment to Pass an Object as a Function's Parameters

--- ES6/app.js
+++ ES6/app.js
@@ -204,12 +204,33 @@ const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
 //  Array.prototype.slice() so that arr is a sub-array of the original array 
 //  source with the first two elements omitted.
 
-const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-function removeFirstTwo(list) {
-  // Only change code below this line
-  const [a, b, ...arr] = list; // Change this line
-  // Only change code above this line
-  return arr;
-}
-const arr = removeFirstTwo(source);
-console.log(arr)
+// const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function removeFirstTwo(list) {
+//   // Only change code below this line
+//   const [a, b, ...arr] = list; // Change this line
+//   // Only change code above this line
+//   return arr;
+// }
+// const arr = removeFirstTwo(source);
+
+
+
+// 
+// Section 14: Use Destructuring Assignment to Pass an Object as a Function's Parameters
+// 
+// Use destructuring assignment within the argument to the
+// function half to send only max and min inside the function.
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = ({ min, max }) => (max + min) / 2.0;
+// Only change code above this line
+console.log(half(stats));
