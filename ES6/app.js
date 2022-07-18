edbb26491a1e8430dfb03fd7ce921f9edaefc0ec
Ran Pol <rnd809@gmail.com>
2022-07-18 07:07:18 -0400

    Complete a Promise with resolve and reject

--- ES6/app.js
+++ ES6/app.js
@@ -443,3 +443,31 @@ import subtract from "./math_functions.js";
 // Promise is a constructor function, so you need to use the new keyword to create one. 
 // It takes a function, as its argument, with two parameters - resolve and reject. 
 // These are methods used to determine the outcome of the promise. 
+// 
+// Create a new promise called makeServerRequest. Pass in a function with resolve and reject
+//  parameters to the constructor.
+
+const makeServerRequest = new Promise((resolve, reject) => {
+
+})
+
+// Section 27: Complete a Promise with resolve and reject
+// 
+// A promise has three states: pending, fulfilled, and rejected. 
+// The promise you created in the last challenge is forever stuck in the pending state 
+// because you did not add a way to complete the promise. The resolve and reject 
+// parameters given to the promise argument are used to do this. resolve is used 
+// when you want your promise to succeed, and reject is used when you want it to fail.
+// 
+// Make the promise handle success and failure. If responseFromServer is true, 
+// call the resolve method to successfully complete the promise. Pass resolve a 
+// string with the value We got the data. If responseFromServer is false, use the 
+// reject method instead and pass it the string: Data not received.
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  if (responseFromServer) {
+    resolve("We got the data.")
+  } else {
+    reject("Data not received.")
+  }
+})
