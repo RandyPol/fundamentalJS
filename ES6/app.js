081265c9f5f97b3f68f883d0f977315336a682aa
Ran Pol <rnd809@gmail.com>
2022-07-18 06:32:33 -0400

    Reuse JavaScript Code Using import

--- ES6/app.js
+++ ES6/app.js
@@ -395,3 +395,13 @@ const lowercaseString = (string) => {
 export {
   uppercaseString, lowercaseString
 }
+
+
+// Section 22: Reuse JavaScript Code Using import
+// 
+// Add the appropriate import statement that will allow the current file to use the
+//  uppercaseString and lowercaseString functions you exported in the previous lesson. 
+//  These functions are in a file called string_functions.js, which is in the same 
+//  directory as the current file.
+
+import { uppercaseString, lowercaseString } from './string_functions.js';
