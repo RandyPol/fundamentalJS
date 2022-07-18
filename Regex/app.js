d1c7d32c045bc282698dc8f859ddaccdc70c8b99
Ran Pol <rnd809@gmail.com>
2022-07-18 19:55:56 -0400

    Match Single Character with Multiple Possibilities

--- Regex/app.js
+++ Regex/app.js
@@ -96,8 +96,25 @@
 
 
 
-let exampleStr = "Let's have fun with regular expressions!";
-let unRegex = /.un/;
-let result = unRegex.test(exampleStr);
+// let exampleStr = "Let's have fun with regular expressions!";
+// let unRegex = /.un/;
+// let result = unRegex.test(exampleStr);
+// console.log(result)
+////////////////////////////////////////////////////////////////////////////
+
+
+
+// Section 8: Match Single Character with Multiple Possibilities
+// 
+// 
+// Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex 
+// to find all the vowels in the string quoteSample.
+
+// Note: Be sure to match both upper- and lowercase vowels.
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+
+let result = quoteSample.match(vowelRegex); // Change this line
 console.log(result)
 ////////////////////////////////////////////////////////////////////////////
