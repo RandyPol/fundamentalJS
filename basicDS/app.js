002749f34df6a3831bd08ed9ef7b10145116de2f
Ran Pol <rnd809@gmail.com>
2022-07-19 19:21:01 -0400

    Section 7: Copy Array Items Using slice()

--- basicDS/app.js
+++ basicDS/app.js
@@ -105,12 +105,33 @@
 //  'DarkSalmon' and 'BlanchedAlmond' in their respective places.
 
 
-function htmlColorNames(arr) {
+// function htmlColorNames(arr) {
+//     // Only change code below this line
+//     arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond')
+//     // Only change code above this line
+//     return arr;
+// }
+
+// console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+// ////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// Section 7: Copy Array Items Using slice()
+// 
+// We have defined a function, forecast, that takes an array as an argument. 
+// Modify the function using slice() to extract information from the argument 
+// array and return a new array that contains the string elements warm and sunny.
+function forecast(arr) {
     // Only change code below this line
-    arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond')
-    // Only change code above this line
-    return arr;
+    return arr.slice(2, 4);
 }
 
-console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
-////////////////////////////////////////////////////////////////////////
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+// ////////////////////////////////////////////////////////////////////////
+
+
