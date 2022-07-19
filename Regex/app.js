085f6d60d013602756659bd7eaf4732303456ca2
Ran Pol <rnd809@gmail.com>
2022-07-18 20:41:04 -0400

    Match Characters that Occur Zero or More Times

--- Regex/app.js
+++ Regex/app.js
@@ -192,3 +192,27 @@
 // let result = difficultSpelling.match(myRegex);
 // console.log(result)
 ////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// Section 13: Match Characters that Occur Zero or More Times
+// 
+// The last challenge used the plus + sign to look for characters 
+// that occur one or more times. There's also an option that matches characters 
+// that occur zero or more times.
+// The character to do this is the asterisk or star: *.
+
+
+const chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!"
+// Only change code below this line
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
+console.log(result)
+
+
+////////////////////////////////////////////////////////////////////////////
