05a0470dc7177df892547c63dcd2f9c6e0143b73
Ran Pol <rnd809@gmail.com>
2022-07-19 12:23:29 -0400

    Catch Off By One Errors When Using Indexing

--- Debugging/app.js
+++ Debugging/app.js
@@ -118,14 +118,32 @@
 
 // Section 9: Catch Arguments Passed in the Wrong Order When Calling a Function
 // 
-function raiseToPower(b, e) {
-    return Math.pow(b, e);
+// function raiseToPower(b, e) {
+//     return Math.pow(b, e);
+// }
+
+// let base = 2;
+// let exp = 3;
+// let power = raiseToPower(base, exp);
+// console.log(power);
+
+////////////////////////////////////////////////////////////////////////
+
+
+// Section 10: Catch Off By One Errors When Using Indexing
+// 
+// Fix the two indexing errors in the following function so 
+// all the numbers 1 through 5 are printed to the console.
+function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Only change code below this line
+    for (let i = 0; i < len; i++) {
+        // Only change code above this line
+        console.log(firstFive[i]);
+    }
 }
 
-let base = 2;
-let exp = 3;
-let power = raiseToPower(base, exp);
-console.log(power);
-
+countToFive();
 
 ////////////////////////////////////////////////////////////////////////
