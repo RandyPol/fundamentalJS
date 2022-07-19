25e5ab1363f562e2dc0f82623d71c3ea30438657
Ran Pol <rnd809@gmail.com>
2022-07-19 10:43:14 -0400

    Use Capture Groups to Search and Replace

--- Regex/app.js
+++ Regex/app.js
@@ -507,9 +507,35 @@
 // Use capture groups in reRegex to match a string that consists 
 // of only the same number repeated exactly three times separated by single spaces.
 
-let repeatNum = "42 42 42";
-let reRegex = /^(\d+)\s\1\s\1$/;
-let result = repeatNum.match(reRegex);
-console.log(result)
+// let repeatNum = "42 42 42";
+// let reRegex = /^(\d+)\s\1\s\1$/;
+// let result = repeatNum.match(reRegex);
+// console.log(result)
 
+////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// Section 31: Use Capture Groups to Search and Replace
+// 
+// Searching is useful. However, you can make searching even more 
+// powerful when it also changes (or replaces) the text you match.
+// 
+// You can search and replace text in a string using .replace() on a string. 
+// The inputs for .replace() is first the regex pattern you want to search for. 
+// The second parameter is the string to replace the match or a function to do something.
+// 
+
+// Write a regex fixRegex using three capture groups that will search for each word in the string one two three. 
+// Then update the replaceText variable to replace one two three with the string three two one and assign the result to the result variable. 
+// Make sure you are utilizing capture groups in the replacement string using the dollar sign ($) syntax.
+
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
+console.log(result)
 ////////////////////////////////////////////////////////////////////////////
