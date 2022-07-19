6fb5350bce38c1ecd116635f3ce0fd70a71c8aae
Ran Pol <rnd809@gmail.com>
2022-07-18 20:00:10 -0400

    Match Letters of the Alphabet

--- Regex/app.js
+++ Regex/app.js
@@ -112,9 +112,25 @@
 
 // Note: Be sure to match both upper- and lowercase vowels.
 
-let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
-let vowelRegex = /[aeiou]/gi; // Change this line
+// let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+// let vowelRegex = /[aeiou]/gi; // Change this line
 
-let result = quoteSample.match(vowelRegex); // Change this line
+// let result = quoteSample.match(vowelRegex); // Change this line
+// console.log(result)
+////////////////////////////////////////////////////////////////////////////
+
+
+// Section 9: Match Letters of the Alphabet
+// 
+// 
+// Inside a character set, you can define a range of characters
+//  to match using a hyphen character: -.
+// 
+// Match all the letters in the string quoteSample.
+// Note: Be sure to match both uppercase and lowercase letters.
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/ig; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
 console.log(result)
 ////////////////////////////////////////////////////////////////////////////
