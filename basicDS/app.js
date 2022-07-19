366e90f9b0fa779c62b5274d2d99ed90fe13f52c
Ran Pol <rnd809@gmail.com>
2022-07-19 19:05:16 -0400

    Section 3: Add Items to an Array with push() and unshift()

--- basicDS/app.js
+++ basicDS/app.js
@@ -20,10 +20,38 @@
 // In order to complete this challenge, set the 2nd position (index 1) 
 // of myArray to anything you want, besides the letter b.
 
-let myArray = ["a", "b", "c", "d"];
-// Only change code below this line
-myArray[1] = 'klk';
-// Only change code above this line
-console.log(myArray);
+// let myArray = ["a", "b", "c", "d"];
+// // Only change code below this line
+// myArray[1] = 'klk';
+// // Only change code above this line
+// console.log(myArray);
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+
+// Section 3: Add Items to an Array with push() and unshift()
+// 
+// We have defined a function, mixedNumbers, which we are passing an 
+// array as an argument. Modify the function by using push() and unshift() 
+// to add 'I', 2, 'three' to the beginning of the array and 7, 'VIII', 9 to 
+// the end so that the returned array contains representations of the 
+// numbers 1-9 in order.
+
+
+// function mixedNumbers(arr) {
+//     // Only change code below this line
+//     arr.unshift('three')
+//     arr.unshift(2)
+//     arr.unshift('I')
+//     arr.push(7)
+//     arr.push('VIII')
+//     arr.push(9)
+//     // Only change code above this line
+//     return arr;
+// }
+
+// console.log(mixedNumbers(['IV', 5, 'six']));
 
 ////////////////////////////////////////////////////////////////////////
