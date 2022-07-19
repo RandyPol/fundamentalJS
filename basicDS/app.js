46eb6657efe0e9f028960a435773346d4a6f6a34
Ran Pol <rnd809@gmail.com>
2022-07-19 19:36:05 -0400

    Section 9: Combine Arrays with the Spread Operator

--- basicDS/app.js
+++ basicDS/app.js
@@ -125,13 +125,61 @@
 // We have defined a function, forecast, that takes an array as an argument. 
 // Modify the function using slice() to extract information from the argument 
 // array and return a new array that contains the string elements warm and sunny.
-function forecast(arr) {
-    // Only change code below this line
-    return arr.slice(2, 4);
+// function forecast(arr) {
+//     // Only change code below this line
+//     return arr.slice(2, 4);
+// }
+
+// // Only change code above this line
+// console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+// ////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// Section 8: Copy an Array with the Spread Operator
+// 
+// We have defined a function, copyMachine which takes arr (an array) 
+// and num (a number) as arguments. The function is supposed to return a 
+// new array made up of num copies of arr. We have done most of the work for 
+// you, but it doesn't work quite right yet. Modify the function using spread 
+// syntax so that it works correctly (hint: another method we have already covered 
+// might come in handy here!).
+
+// function copyMachine(arr, num) {
+//     let newArr = [];
+//     while (num >= 1) {
+//       // Only change code below this line
+//   newArr.push([...arr])
+//       // Only change code above this line
+//       num--;
+//     }
+//     return newArr;
+//   }
+
+//   console.log(copyMachine([true, false, true], 2));
+
+// // ////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// Section 9: Combine Arrays with the Spread Operator
+// 
+// We have defined a function spreadOut that returns the variable sentence. 
+// Modify the function using the spread operator so that it returns the array 
+// ['learning', 'to', 'code', 'is', 'fun'].
+
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ["learning", ...fragment, "is", "fun"]
+    return sentence;
 }
 
-// Only change code above this line
-console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
-// ////////////////////////////////////////////////////////////////////////
+console.log(spreadOut());
+// // ////////////////////////////////////////////////////////////////////////
 
 
