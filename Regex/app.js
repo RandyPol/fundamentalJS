2c68a7856a111e78e19b98346c7e70bce69d9b9f
Ran Pol <rnd809@gmail.com>
2022-07-18 20:07:36 -0400

    Match Single Characters Not Specified

--- Regex/app.js
+++ Regex/app.js
@@ -148,10 +148,27 @@
 // between 2 and 6. Remember to include the appropriate flags in the regex.match(alphabetRegex);
 
 
-let quoteSample = "Blueberry 3.141592653s are delicious.";
-let myRegex = /[h-s2-6]/ig;
+// let quoteSample = "Blueberry 3.141592653s are delicious.";
+// let myRegex = /[h-s2-6]/ig;
+// let result = quoteSample.match(myRegex);
+// console.log(result)
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+
+// Section 11: Match Single Characters Not Specified
+// 
+// 
+// To create a negated character set, you place a caret character (^) 
+// after the opening bracket and before the characters you do not want to match.
+// Create a single regex that matches all characters that are not a number or a vowel.
+//  Remember to include the appropriate flags in the regex.
+
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou0-9]/gi; // Change this line
 let result = quoteSample.match(myRegex);
 console.log(result)
-
 
 ////////////////////////////////////////////////////////////////////////////
