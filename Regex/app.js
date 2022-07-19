52452004eb55bef002338fb1c4fd6f5a2ac5dfdc
Ran Pol <rnd809@gmail.com>
2022-07-18 21:43:02 -0400

    Find One or More Criminals in a Hunt

--- Regex/app.js
+++ Regex/app.js
@@ -236,3 +236,18 @@ let result = text.match(myRegex);
 console.log(result)
 
 ////////////////////////////////////////////////////////////////////////////
+
+
+
+// Section 15: Find One or More Criminals in a Hunt
+
+// 
+// Time to pause and test your new regex writing skills. 
+// A group of criminals escaped from jail and ran away, but 
+// you don't know how many. However, you do know that they stay 
+// close together when they are around other people. You are responsible 
+// for finding all of the criminals at once.
+// 
+
+let reCriminals = /C+/g; // Change this line
+////////////////////////////////////////////////////////////////////////////
