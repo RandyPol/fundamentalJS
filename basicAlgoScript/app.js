c461b25707097c460baad12f0448ab1ad8b3a0eb
Ran Pol <rnd809@gmail.com>
2022-07-20 06:05:00 -0400

    Section 12: Slice and Splice

--- basicAlgoScript/app.js
+++ basicAlgoScript/app.js
@@ -244,8 +244,27 @@
 // }
 
 // console.log(titleCase("I'm a little tea pot"));
+// ////////////////////////////////////////////////////////////////////////
 
 
 
+// 
+// Section 12: Slice and Splice
+// 
+// You are given two arrays and an index.
+
+// Copy each element of the first array into the second array, in order.
+
+// Begin inserting elements at index n of the second array.
+
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+// function frankenSplice(arr1, arr2, n) {
+//     let newArr = arr2.slice()
+//     newArr.splice(n, 0, ...arr1);
+//     return newArr
+// }
+
+// console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
 
 // ////////////////////////////////////////////////////////////////////////
