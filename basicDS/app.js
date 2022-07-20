2ba7236436f8f1e6426ffcecf890058534ca1f20
Ran Pol <rnd809@gmail.com>
2022-07-19 22:19:07 -0400

    Section 11: Iterate Through All an Array's Items Using For Loops :D

--- basicDS/app.js
+++ basicDS/app.js
@@ -193,17 +193,46 @@
 // indexOf() so that it returns true if the passed element exists on the array, 
 // and false if it does not.
 
-function quickCheck(arr, elem) {
-    // Only change code below this line
-    console.log(arr.indexOf(elem))
-    return arr.indexOf(elem) === -1 ? console.log(elem, false) : console.log(elem, true);
-    // Only change code above this line
-}
+// function quickCheck(arr, elem) {
+//     // Only change code below this line
+//     return arr.indexOf(elem) === -1 ? false : true;
+//     // Only change code above this line
+// }
 
-console.log(quickCheck(['squash', 'onions', 'shallots'], 'onions'));
+// console.log(quickCheck(['squash', 'onions', 'shallots'], 'onions'));
 
 
 ////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// Section 11: Iterate Through All an Array's Items Using For Loops
+// 
+// We have defined a function, filteredArray, which takes arr, a nested array, 
+// and elem as arguments, and returns a new array. elem represents an element 
+// that may or may not be present on one or more of the arrays nested within arr. 
+// Modify the function, using a for loop, to return a filtered version of the 
+// passed array such that any array nested within arr containing elem has been 
+// removed.
+
+// function filteredArray(arr, elem) {
+//     let newArr = [];
+//     // Only change code below this line
+//     for (let i = 0; i < arr.length; i++) {
+//         // console.log(arr[i].indexOf(elem))
+//         if (arr[i].indexOf(elem) === -1) {
+//             newArr.push(arr[i])
+//         } 
+//     }
+//     // Only change code above this line
+//     return newArr;
+// }
+
+////////////////////////////////////////////////////////////////////////////
+
 
 
 
