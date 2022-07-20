82960cb531069bfd9f36730e0cde59aa447911d4
Ran Pol <rnd809@gmail.com>
2022-07-20 04:33:21 -0400

    Section 7: Repeat a String Repeat a String

--- basicAlgoScript/app.js
+++ basicAlgoScript/app.js
@@ -126,8 +126,6 @@
 
 
 
-
-
 // 
 // Section 6: Confirm the Ending
 // 
@@ -143,5 +141,28 @@
 // }
 
 // console.log(confirmEnding("Bastian", "n"));
+
+// ////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// 
+// Section 7: Repeat a String Repeat a String
+// 
+// Repeat a given string str (first argument) for num times (second argument). 
+// Return an empty string if num is not a positive number. For the purpose of this 
+// challenge, do not use the built-in .repeat() method.
+
+function repeatStringNumTimes(str, num) {
+    return num >= 0 ? Array(++num).join(str) : ''
+}
+
+console.log(repeatStringNumTimes("abc", 3));
+
+
+
 
 // ////////////////////////////////////////////////////////////////////////
