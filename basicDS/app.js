e733491c70d60404ef3e240e790be6e57396d843
Ran Pol <rnd809@gmail.com>
2022-07-19 22:39:34 -0400

    Section 12: Create complex multi-dimensional arrays

--- basicDS/app.js
+++ basicDS/app.js
@@ -236,3 +236,29 @@
 
 
 
+
+// Section 12: Create complex multi-dimensional arrays
+// 
+// We have defined a variable, myNestedArray, set equal to an array. 
+// Modify myNestedArray, using any combination of strings, numbers, and 
+// booleans for data elements, so that it has exactly five levels of 
+// depth (remember, the outer-most array is level 1). Somewhere on the third 
+// level, include the string deep, on the fourth level, include the string 
+// deeper, and on the fifth level, include the string deepest.
+
+let myNestedArray = [
+    // Only change code below this line
+    ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+    [[["deep", ["deeper", ["deepest"]]]]],
+    ['concat', false, true, 'spread', 'array'],
+    ['mutate', 1327.98, 'splice', 'slice', 'push'],
+    ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+    // Only change code above this line
+];
+
+myNestedArray[2] = [[[[["deepest"]]]]]
+console.log(myNestedArray)
+////////////////////////////////////////////////////////////////////////////
+
+
+
