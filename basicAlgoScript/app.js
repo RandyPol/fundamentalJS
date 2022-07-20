daf977c15f76e86191e52ffb1376556b3bd5d5f8
Ran Pol <rnd809@gmail.com>
2022-07-20 02:47:17 -0400

    Section 2: Reverse a String

--- basicAlgoScript/app.js
+++ basicAlgoScript/app.js
@@ -12,15 +12,36 @@
 //   above to help convert the Celsius temperature to Fahrenheit.
 
 
-function convertCtoF(celsius) {
-    let fahrenheit = celsius * (9 / 5) + 32
-    return fahrenheit;
-}
+// function convertCtoF(celsius) {
+//     let fahrenheit = celsius * (9 / 5) + 32
+//     return fahrenheit;
+// }
 
-convertCtoF(30);
+// convertCtoF(30);
 
 
+////////////////////////////////////////////////////////////////////////
 
+
+// 
+// Section 2: Reverse a String
+// 
+// Reverse the provided string.
+
+// You may need to turn the string into an array before you can reverse it.
+
+// Your result must be a string.
+
+
+// function reverseString(str) {
+//     let revStr = '';
+//     for (let i = (str.length - 1); i >= 0; i--) {
+//         revStr += str[i]
+//     }
+//     return revStr;
+// }
+
+// console.log(reverseString("hello"));
 
 
 ////////////////////////////////////////////////////////////////////////
