1bc1a6a73cac20a99310b17715695d73d8498cb2
Ran Pol <rnd809@gmail.com>
2022-07-19 23:43:59 -0400

    Section 17: Check if an Object has a Property

--- basicDS/app.js
+++ basicDS/app.js
@@ -343,3 +343,76 @@
 ////////////////////////////////////////////////////////////////////////////
 
 
+
+
+
+
+// Section 16: Use the delete Keyword to Remove Object Properties
+// 
+// Use the delete keyword to remove the oranges, plums, and strawberries keys from the foods object.
+
+
+// let foods = {
+//     apples: 25,
+//     oranges: 32,
+//     plums: 28,
+//     bananas: 13,
+//     grapes: 35,
+//     strawberries: 27
+//   };
+
+//   // Only change code below this line
+//   delete foods["oranges"];
+//   delete foods["plums"];
+//   delete foods["strawberries"];
+//   // Only change code above this line
+
+//   console.log(foods);
+
+////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// Section 17: Check if an Object has a Property
+// 
+// Finish writing the function so that it returns true if the object passed 
+// to it contains all four names, Alan, Jeff, Sarah and Ryan and returns false otherwise.
+
+let users = {
+    Alan: {
+        age: 27,
+        online: true
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: true
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function isEveryoneHere(userObj) {
+    let allIn = true;
+    // Only change code below this line
+    "Alan" in userObj ? '' : allIn = false;
+    "Jeff" in userObj ? '' : allIn = false;
+    "Sarah" in userObj ? '' : allIn = false;
+    "Ryan" in userObj ? '' : allIn = false;
+    return allIn
+    // Only change code above this line
+}
+
+console.log(isEveryoneHere(users));
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
