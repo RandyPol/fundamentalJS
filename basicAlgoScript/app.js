e3fac074eabf1ccd704cbe2ca8e8666b9143498b
Ran Pol <rnd809@gmail.com>
2022-07-20 05:18:54 -0400

    Section 9: Finders Keepers

--- basicAlgoScript/app.js
+++ basicAlgoScript/app.js
@@ -156,13 +156,55 @@
 // Return an empty string if num is not a positive number. For the purpose of this 
 // challenge, do not use the built-in .repeat() method.
 
-function repeatStringNumTimes(str, num) {
-    return num >= 0 ? Array(++num).join(str) : ''
-}
+// function repeatStringNumTimes(str, num) {
+//     return num >= 0 ? Array(++num).join(str) : ''
+// }
 
-console.log(repeatStringNumTimes("abc", 3));
+// console.log(repeatStringNumTimes("abc", 3));
 
 
+// ////////////////////////////////////////////////////////////////////////
+
+
+
+// 
+// Section 8: Truncate a String
+// 
+// Truncate a string (first argument) if it is longer than the given maximum string 
+// length (second argument). Return the truncated string with a ... ending.
+
+// function truncateString(str, num) {
+//     if (str.length <= num) {
+//         return str
+//     } else {
+//         return str.slice(0, num) + '...'
+//     }
+// }
+
+// console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+
+// ////////////////////////////////////////////////////////////////////////
+
+
+
+// 
+// Section 9: Finders Keepers
+// 
+// Create a function that looks through an array arr and returns the first element 
+// in it that passes a 'truth test'. This means that given an element x, the 'truth test' 
+// is passed if func(x) is true. If no element passes the test, return undefined.
+
+
+// function findElement(arr, func) {
+//     for (let item of arr) {
+//         if (func(item)) {
+//             return item
+//         }
+//     }
+//     return undefined
+// }
+
+// console.log(findElement([10, 2, 3, 4], num => num % 2 === 0))
 
 
 // ////////////////////////////////////////////////////////////////////////
