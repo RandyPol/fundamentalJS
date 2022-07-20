e91d5489a92856957d25ff26fb463547d1e56a9f
Ran Pol <rnd809@gmail.com>
2022-07-19 23:00:52 -0400

    Section 15: Access Property Names with Bracket Notation

--- basicDS/app.js
+++ basicDS/app.js
@@ -296,20 +296,49 @@
 // Here we've defined an object userActivity, which includes another object nested within it. 
 // Set the value of the online key to 45.
 
-let userActivity = {
-    id: 23894201352,
-    date: 'January 1, 2017',
-    data: {
-        totalUsers: 51,
-        online: 42
-    }
-};
+// let userActivity = {
+//     id: 23894201352,
+//     date: 'January 1, 2017',
+//     data: {
+//         totalUsers: 51,
+//         online: 42
+//     }
+// };
 
-// Only change code below this line
-userActivity.data.online = 45;
-// Only change code above this line
+// // Only change code below this line
+// userActivity.data.online = 45;
+// // Only change code above this line
 
-console.log(userActivity);
+// console.log(userActivity);
+
+////////////////////////////////////////////////////////////////////////////
+
+
+
+// Section 15: Access Property Names with Bracket Notation
+// 
+// We've defined a function, checkInventory, which receives a scanned item as an argument.
+//  Return the current value of the scannedItem key in the foods object. You can assume that 
+//  only valid keys will be provided as an argument to checkInventory.
+
+
+// let foods = {
+//     apples: 25,
+//     oranges: 32,
+//     plums: 28,
+//     bananas: 13,
+//     grapes: 35,
+//     strawberries: 27
+// };
+
+// function checkInventory(scannedItem) {
+//     // Only change code below this line
+//     return foods[scannedItem]
+//     // Only change code above this line
+// }
+
+// console.log(checkInventory("apples"));
+
 
 ////////////////////////////////////////////////////////////////////////////
 
