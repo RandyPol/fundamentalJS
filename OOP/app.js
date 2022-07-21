f5d9cca7052ffc08763c5b94949a4df2437363dc
Ran Pol <rnd809@gmail.com>
2022-07-21 06:03:29 -0400

    Section 13: Change the Prototype to a New Object

--- OOP/app.js
+++ OOP/app.js
@@ -204,3 +204,33 @@
 
 
 ////////////////////////////////////////////////////////////////////////
+
+
+// 
+// Section 13: Change the Prototype to a New Object
+// 
+// 
+// A more efficient way is to set the prototype to a new object that already 
+// contains the properties. This way, the properties are added all at once:
+
+// Add the property numLegs and the two methods eat() and describe() to the prototype 
+// of Dog by setting the prototype to a new object.
+
+
+
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype = {
+    // Only change code below this line
+    numLegs: 4,
+    eat: function () {
+        console.log("nom nom nom");
+    },
+    describe: function () {
+        console.log("My name is " + this.name);
+    }
+};
+
+////////////////////////////////////////////////////////////////////////
