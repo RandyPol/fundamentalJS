139abb4eaa78ff80628680fbe5b36204af6d85ef
Ran Pol <rnd809@gmail.com>
2022-07-21 05:20:11 -0400

    Section 8: Verify an Object's Constructor with instanceof

--- OOP/app.js
+++ OOP/app.js
@@ -85,3 +85,18 @@
 // let terrier = new Dog("terrier", "Red")
 // console.log(terrier)
 ////////////////////////////////////////////////////////////////////////
+
+
+
+// Section 8: Verify an Object's Constructor with instanceof
+// 
+// Create a new instance of the House constructor, calling it myHouse and passing a number of bedrooms. 
+// Then, use instanceof to verify that it is an instance of House.
+
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+}
+
+let myHouse = new House(10);
+console.log(myHouse instanceof House)
+////////////////////////////////////////////////////////////////////////
