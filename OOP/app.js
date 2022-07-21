63664aef0a1b0e36b1907707988da689fbfe9758
Ran Pol <rnd809@gmail.com>
2022-07-21 06:09:48 -0400

    Section 14: Remember to Set the Constructor Property when Changing the Prototype

--- OOP/app.js
+++ OOP/app.js
@@ -218,19 +218,45 @@
 
 
 
-function Dog(name) {
-    this.name = name;
-}
+// function Dog(name) {
+//     this.name = name;
+// }
 
-Dog.prototype = {
-    // Only change code below this line
-    numLegs: 4,
-    eat: function () {
-        console.log("nom nom nom");
-    },
-    describe: function () {
-        console.log("My name is " + this.name);
-    }
-};
+// Dog.prototype = {
+//     // Only change code below this line
+//     numLegs: 4,
+//     eat: function () {
+//         console.log("nom nom nom");
+//     },
+//     describe: function () {
+//         console.log("My name is " + this.name);
+//     }
+// };
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+
+// 
+// Section 14: Remember to Set the Constructor Property when Changing the Prototype
+// 
+// 
+// There is one crucial side effect of manually setting the prototype to a new object. 
+// It erases the constructor property! This property can be used to check which constructor function 
+// created the instance, but since the property has been overwritten, it now gives false results:
+// Define the constructor property on the Dog prototype.
+
+
+// Dog.prototype = {
+//     constructor: Dog,
+//       numLegs: 4,
+//       eat: function() {
+//         console.log("nom nom nom");
+//       },
+//       describe: function() {
+//         console.log("My name is " + this.name);
+//       }
+//     };
 
 ////////////////////////////////////////////////////////////////////////
