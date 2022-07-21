23c27040a0d9832ada20d95bc28c6d535808c2a0
Ran Pol <rnd809@gmail.com>
2022-07-21 05:16:47 -0400

    Section 7: Extend Constructors to Receive Arguments

--- OOP/app.js
+++ OOP/app.js
@@ -58,12 +58,30 @@
 // Use the Dog constructor from the last lesson to create a new instance of Dog, 
 // assigning it to a variable hound.
 
-function Dog(name, color, numLegs) {
-    this.name = name;
-    this.color = color;
-    this.numLegs = numLegs;
-}
+// function Dog(name, color, numLegs) {
+//     this.name = name;
+//     this.color = color;
+//     this.numLegs = numLegs;
+// }
 
-let hound = new Dog('Tommy', "Red", 5)
-console.log(dogOne)
+// let hound = new Dog('Tommy', "Red", 5)
+// console.log(dogOne)
+////////////////////////////////////////////////////////////////////////
+
+
+// Section 7: Extend Constructors to Receive Arguments
+// 
+// Create another Dog constructor. This time, set it up to take the parameters name and color,
+// and have the property numLegs fixed at 4. Then create a new Dog saved in a variable terrier. 
+// Pass it two strings as arguments for the name and color properties.
+
+
+// function Dog(name, color) {
+//     this.name = name;
+//     this.color = color;
+//     this.numLegs = 4;
+// }
+
+// let terrier = new Dog("terrier", "Red")
+// console.log(terrier)
 ////////////////////////////////////////////////////////////////////////
