4e27c854670b007960c9709f6dc0e494d40084e1
Ran Pol <rnd809@gmail.com>
2022-07-22 19:22:45 -0400

    Section 21: Add Methods After Inheritance

--- OOP/app.js
+++ OOP/app.js
@@ -389,21 +389,54 @@
 // 
 // Fix the code so duck.constructor and beagle.constructor return their respective constructors.
 
-function Animal() { }
-function Bird() { }
-function Dog() { }
+// function Animal() { }
+// function Bird() { }
+// function Dog() { }
 
-Bird.prototype = Object.create(Animal.prototype);
-Dog.prototype = Object.create(Animal.prototype);
+// Bird.prototype = Object.create(Animal.prototype);
+// Dog.prototype = Object.create(Animal.prototype);
+
+// // Only change code below this line
+
+// Bird.prototype.constructor = Bird;
+// Dog.prototype.constructor = Dog;
+
+// let duck = new Bird();
+// let beagle = new Dog();
+
+////////////////////////////////////////////////////////////////////////
+
+
+// 
+// Section 21: Add Methods After Inheritance
+// 
+// 
+// A constructor function that inherits its prototype object from a supertype constructor 
+// function can still have its own methods in addition to inherited methods.
+
+// Add all necessary code so the Dog object inherits from Animal and the Dog's prototype 
+// constructor is set to Dog. Then add a bark() method to the Dog object so that beagle 
+// can both eat() and bark(). The bark() method should print Woof! to the console.
+
+function Animal() { }
+Animal.prototype.eat = function () { console.log("nom nom nom"); };
+
+function Dog() { }
 
 // Only change code below this line
 
-
-Bird.prototype.constructor = Bird;
+Dog.prototype = Object.create(Animal.prototype);
 Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function () {
+    console.log("Woof!");
+}
 
-let duck = new Bird();
+
+// Only change code above this line
+
 let beagle = new Dog();
+
+
 
 ////////////////////////////////////////////////////////////////////////
 
