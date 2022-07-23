b8b5e573429d8e5e9f999ba7c1851786cf64c0a5
Ran Pol <rnd809@gmail.com>
2022-07-23 19:07:44 -0400

    Section 2: Understand Functional Programming Terminology: Higher Order Functions & Callbacks, Lambda

--- funtionalPro/app.js
+++ funtionalPro/app.js
@@ -20,14 +20,50 @@
 
 
 // Function that returns a string representing a cup of green tea
-const prepareTea = () => 'greenTea';
+// const prepareTea = () => 'greenTea';
+
+// /*
+// Given a function (representing the tea type) and number of cups needed, the
+// following function returns an array of strings (each representing a cup of
+// a specific type of tea).
+// */
+// const getTea = (numOfCups) => {
+//     const teaCups = [];
+
+//     for (let cups = 1; cups <= numOfCups; cups += 1) {
+//         const teaCup = prepareTea();
+//         teaCups.push(teaCup);
+//     }
+//     return teaCups;
+// };
+
+// // Only change code below this line
+// const tea4TeamFCC = getTea(40);
+// // Only change code above this line
+
+// ////////////////////////////////////////////////////////////////////////
+
+// 
+// Section 2: Understand Functional Programming Terminology
+// 
+// Prepare 27 cups of green tea and 13 cups of black tea and store them 
+// in tea4GreenTeamFCC and tea4BlackTeamFCC variables, respectively. 
+// Note that the getTea function has been modified so it now takes a function as the first argument.
+// Note: The data (the number of cups of tea) is supplied as the last argument. 
+// We'll discuss this more in later lessons.
+
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
 
 /*
 Given a function (representing the tea type) and number of cups needed, the
 following function returns an array of strings (each representing a cup of
 a specific type of tea).
 */
-const getTea = (numOfCups) => {
+const getTea = (prepareTea, numOfCups) => {
     const teaCups = [];
 
     for (let cups = 1; cups <= numOfCups; cups += 1) {
@@ -38,9 +74,12 @@ const getTea = (numOfCups) => {
 };
 
 // Only change code below this line
-const tea4TeamFCC = getTea(40);
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
 // Only change code above this line
 
-
-
+console.log(
+    tea4GreenTeamFCC,
+    tea4BlackTeamFCC
+);
 // ////////////////////////////////////////////////////////////////////////
