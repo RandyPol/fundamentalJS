2f243e5ecef5372a88cf45209d8ca3d767f71f74
Ran Pol <rnd809@gmail.com>
2022-07-23 23:23:28 -0400

    Section 8: Implement map on a Prototype

--- funtionalPro/app.js
+++ funtionalPro/app.js
@@ -377,3 +377,30 @@
 
 //////////////////////////////////////////////////////////////////////////
 
+
+// 
+// Section 8: Implement map on a Prototype
+// 
+// 
+// 
+// Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). 
+// You should not use the built-in map method. The Array instance can be accessed in the myMap method using this.
+
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function (callback) {
+    const newArray = [];
+    // Only change code below this line
+    for (let item of this) {
+        newArray.push(callback(item))
+    }
+    // Only change code above this line
+    return newArray;
+};
+
+const new_s = s.myMap(function (item) {
+    return item * 2;
+});
+console.log(new_s)
+//////////////////////////////////////////////////////////////////////////
