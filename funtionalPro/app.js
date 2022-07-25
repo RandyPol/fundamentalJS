50807547de8ba9b78a4398ef5b9e726341f67583
Ran Pol <rnd809@gmail.com>
2022-07-24 21:11:39 -0400

    Section 20: Apply Functional Programming to Convert Strings to URL Slugs

--- funtionalPro/app.js
+++ funtionalPro/app.js
@@ -781,13 +781,41 @@
 // words in the string str. The function should return a string. For example, I-like-Star-Wars 
 // would be converted to I like Star Wars. For this challenge, do not use the replace method.
 
-function sentensify(str) {
-    // Only change code below this line
+// function sentensify(str) {
+//     // Only change code below this line
 
-    return str.split(/\W/).join(" ")
-    // Only change code above this line
+//     return str.split(/\W/).join(" ")
+//     // Only change code above this line
+// }
+
+// console.log(sentensify("May-the-force-be-with-you"));
+
+//////////////////////////////////////////////////////////////////////////
+
+
+// 
+// Section 20: Apply Functional Programming to Convert Strings to URL Slugs
+// 
+// 
+// Fill in the urlSlug function so it converts a string title and returns the hyphenated version 
+// for the URL. You can use any of the methods covered in this section, and don't use replace. 
+// Here are the requirements:
+
+// The input is a string with spaces and title-cased words
+
+// The output is a string with the spaces between words replaced by a hyphen (-)
+
+// The output should be all lower-cased letters
+
+// The output should not have any spaces
+
+// Only change code below this line
+function urlSlug(title) {
+    return title.trim().toLowerCase().split(/\W+/).join("-")
 }
+// Only change code above this line
+console.log(urlSlug(" Winter Is  Coming"));
 
-console.log(sentensify("May-the-force-be-with-you"));
+
 
 //////////////////////////////////////////////////////////////////////////
