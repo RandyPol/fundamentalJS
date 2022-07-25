ecd1b5baa8dd4633ea24212dc460f80b6a9406fa
Ran Pol <rnd809@gmail.com>
2022-07-24 20:05:01 -0400

    Section 16: Sort an Array Alphabetically using the sort Method

--- funtionalPro/app.js
+++ funtionalPro/app.js
@@ -669,8 +669,6 @@
 
 //////////////////////////////////////////////////////////////////////////
 
-
-
 // 
 // Section 15: Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
 // 
@@ -687,14 +685,35 @@
 // Note: Your function should not use any kind of for or while loops or the forEach() function.
 
 
-const squareList = arr => {
-    // Only change code below this line
-    return arr.filter(num => Number.isInteger(num) && num > 0).map(num => num ** 2)
-    // return arr;
-    // Only change code above this line
-};
+// const squareList = arr => {
+//     // Only change code below this line
+//     return arr.filter(num => Number.isInteger(num) && num > 0).map(num => num ** 2)
+//     // return arr;
+//     // Only change code above this line
+// };
 
-const squaredIntegers = squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]);
-console.log(squaredIntegers);
+// const squaredIntegers = squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]);
+// console.log(squaredIntegers);
+
+//////////////////////////////////////////////////////////////////////////
+
+// 
+// Section 16: Sort an Array Alphabetically using the sort Method
+// 
+// 
+// Use the sort method in the alphabeticalOrder function to sort the elements of 
+// arr in alphabetical order. The function should return the sorted array.
+
+function alphabeticalOrder(arr) {
+    // Only change code below this line
+    return arr.sort((a, b) => {
+        return a.charCodeAt(0) - b.charCodeAt(0)
+    })
+    // return arr
+    // Only change code above this line
+}
+
+console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]))
+
 
 //////////////////////////////////////////////////////////////////////////
