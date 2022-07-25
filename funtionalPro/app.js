403095b448289458d98ab9d6a4a072024d9fc4d3
Ran Pol <rnd809@gmail.com>
2022-07-24 21:16:31 -0400

    Section 22: Use the some Method to Check that Any Elements in an Array Meet a Criteria

--- funtionalPro/app.js
+++ funtionalPro/app.js
@@ -825,13 +825,31 @@
 // Use the every method inside the checkPositive function to check if every element in arr 
 // is positive. The function should return a Boolean value.
 
+// function checkPositive(arr) {
+//     // Only change code below this line
+
+//     return arr.every(num => num > 0)
+//     // Only change code above this line
+// }
+
+// console.log(checkPositive([1, 2, 3, -4, 5]));
+
+//////////////////////////////////////////////////////////////////////////
+
+// 
+// Section 22: Use the some Method to Check that Any Elements in an Array Meet a Criteria
+// 
+// 
+// Use the some method inside the checkPositive function to check if any element in arr is positive. 
+// The function should return a Boolean value.
+
 function checkPositive(arr) {
     // Only change code below this line
+    return arr.some(num => num > 0)
 
-    return arr.every(num => num > 0)
     // Only change code above this line
 }
 
-console.log(checkPositive([1, 2, 3, -4, 5]));
+checkPositive([1, 2, 3, -4, 5]);
 
 //////////////////////////////////////////////////////////////////////////
