31d1bfef8121fbb17140ca80995c6d380e40a62b
Ran Pol <rnd809@gmail.com>
2022-07-26 03:08:00 -0400

    Section 3: Seek and Destroy

--- intermediateAlgo/app.js
+++ intermediateAlgo/app.js
@@ -54,3 +54,23 @@
 // console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
 
 ////////////////////////////////////////////////////////////////////////
+
+// 
+// Section 3: Seek and Destroy
+// 
+// 
+// 
+//You will be provided with an initial array (the first argument in the destroyer function), 
+// followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+// Note: You have to use the arguments object.
+
+
+function destroyer(arr) {
+    return arguments[0].filter(item => ![...arguments].slice(1).includes(item))
+}
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+
+////////////////////////////////////////////////////////////////////////
