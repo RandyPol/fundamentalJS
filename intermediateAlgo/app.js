775b35aa80578dede07ffe20fb60c232f962acd1
Ran Pol <rnd809@gmail.com>
2022-07-26 06:07:34 -0400

    Section 4: Wherefore art thou

--- intermediateAlgo/app.js
+++ intermediateAlgo/app.js
@@ -65,12 +65,78 @@
 
 // Note: You have to use the arguments object.
 
+// function destroyer(arr) {
+//     return arguments[0].filter(item => ![...arguments].slice(1).includes(item))
+// }
 
-function destroyer(arr) {
-    return arguments[0].filter(item => ![...arguments].slice(1).includes(item))
-}
+// console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
 
-console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+////////////////////////////////////////////////////////////////////////
 
+// 
+// Section 4: Wherefore art thou
+// 
+// 
+// 
+// Make a function that looks through an array of objects (first argument) and returns an array of all 
+// objects that have matching name and value pairs (second argument). Each name and value pair of the source 
+// object has to be present in the object from the collection if it is to be included in the returned array.
+
+// Note: You have to use the arguments object.
+
+// let listObjt = [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }];
+// let lastItem = { last: "Capulet" };
+
+// function whatIsInAName(collection, source) {
+//     const arr = [];
+//     // Only change code below this line
+//     let compareKeys = Object.keys(source)
+//     collection.forEach(element => {
+//         let result = compareKeys.every(item => Object.keys(element).includes(item))
+
+//         if (result) {
+//             for (let key in source) {
+//                 if (!(element[key] === source[key])) {
+//                     result = 0;
+//                 }
+//             }
+//         }
+//         if (result) {
+//             arr.push(element)
+//         }
+
+//     });
+//     // Only change code above this line
+//     return arr;
+// }
+
+// console.log(whatIsInAName(listObjt, lastItem));
+
+
+
+// function whatIsInAName(collection, source) {
+//     const souceKeys = Object.keys(source);
+  
+//     // filter the collection
+//     return collection.filter(obj => {
+//       for (let i = 0; i < souceKeys.length; i++) {
+//         if (!obj.hasOwnProperty(souceKeys[i]) || obj[souceKeys[i]] !== source[souceKeys[i]]) {
+//           return false;
+//         }
+//       }
+//       return true;
+//     });
+//   }
+  
+//   function whatIsInAName(collection, source) {
+//     // "What's in a name? that which we call a rose
+//     // By any other name would smell as sweet.”
+//     // -- by William Shakespeare, Romeo and Juliet
+//     const sourceKeys = Object.keys(source);
+  
+//     return collection
+//       .filter(obj => sourceKeys
+//                        .every(key => obj.hasOwnProperty(key) && obj[key] === source[key]));
+//   }
 
 ////////////////////////////////////////////////////////////////////////
