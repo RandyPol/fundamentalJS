90e3f805dd6dd8ebf951e11e7f5d297b83abdd08
Ran Pol <rnd809@gmail.com>
2022-08-01 23:30:26 -0400

    Section 20: Make a Person

--- intermediateAlgo/app.js
+++ intermediateAlgo/app.js
@@ -574,26 +574,79 @@
 // Calling this returned function with a single argument will then return the sum:
 
 
-function addTogether(a, b) {
-    if (arguments.length === 1 && typeof a === 'number') {
-        return function (x) {
-            if (typeof x !== 'number') {
-                return undefined
-            } else {
-                return x + a
-            }
-        }
-    } else if (typeof a === 'number' && typeof b === 'number') {
-        return a + b
-    } else {
-        return undefined
-    }
-}
+// function addTogether(a, b) {
+//     if (arguments.length === 1 && typeof a === 'number') {
+//         return function (x) {
+//             if (typeof x !== 'number') {
+//                 return undefined
+//             } else {
+//                 return x + a
+//             }
+//         }
+//     } else if (typeof a === 'number' && typeof b === 'number') {
+//         return a + b
+//     } else {
+//         return undefined
+//     }
+// }
 
-console.log(addTogether(2)(2))
+// console.log(addTogether(2)(2))
 
 ////////////////////////////////////////////////////////////////////////
 
+
+
+// 
+// Section 20: Make a Person
+// 
+// 
+// 
+// Fill in the object constructor with the following methods below:
+// getFirstName()
+// getLastName()
+// getFullName()
+// setFirstName(first)
+// setLastName(last)
+// setFullName(firstAndLast)
+// Run the tests to see the expected output for each method. The methods that take an 
+// argument must accept only one argument and it has to be a string. These methods must 
+// be the only available means of interacting with the object.
+
+
+// const Person = function (firstAndLast) {
+//     // Only change code below this line
+//     // Complete the method below and implement the others similarly
+//     let [firstName, lastName] = firstAndLast.split(' ');
+//     this.getFullName = function () {
+//         return `${firstName} ${lastName}`;
+//     };
+//     this.getFirstName = function () {
+//         return firstName
+//     };
+//     this.getLastName = function () {
+//         return lastName
+//     };
+
+//     this.setFirstName = function (first) {
+//         firstName = first;
+//     };
+//     this.setLastName = function (last) {
+//         lastName = last;
+//     };
+
+//     this.setFullName = function (fullName) {
+//         [firstName, lastName] = fullName.split(" ");
+//     };
+
+// };
+
+// const bob = new Person('Bob Ross');
+// console.log(bob.getFullName());
+
+// console.log(Object.keys(bob).length)
+
+
+////////////////////////////////////////////////////////////////////////
 
 
 
